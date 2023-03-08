f3aa8954a51ca0693c58c063208b9c2a7a5d77b4
Alia Ajina <aliaajina@gmail.com>
2023-03-08 08:24:42 -0800

    adding some final documentation to Assignment 4

--- R/fish_fnct.R
+++ R/fish_fnct.R
@@ -1,7 +1,14 @@
+# Compute fish function: compute msot common fish fished, least common, and total number of fish fished
+# @param my_data is your own dataset, in my case it is data of fish
+
 fish_fnct = function(my_data) { 
-  most_common_fish <- names(which.max(summary(my_data)))
-  rarest_fish <- names(which.min(summary(my_data)))
-  total_num <- sum(summary(my_data))
+  
+  data_vect <- as.vector(my_data[,1])
+  data_fact <- as.factor(data_vect)
+  
+  most_common_fish <- names(which.max(summary(data_fact)))
+  rarest_fish <- names(which.min(summary(data_fact)))
+  total_num <- sum(summary(data_fact))
   final_list <- c(most_common_fish,rarest_fish,total_num)
   
   return(final_list)
